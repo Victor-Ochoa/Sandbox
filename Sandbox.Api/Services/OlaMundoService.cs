8c026bd01ff80b735690085e4337e7f0217e9843
Victor Ochoa <Victor.Ochoa@POSSIBLE.com>
2020-11-14 01:30:43 -0300

    adicionado a api e o teste

---
+++ Sandbox.Api/Services/OlaMundoService.cs
@@ -0,0 +1,15 @@
+﻿using Grpc.Core;
+using Sandbox.Protos;
+using System.Threading.Tasks;
+
+namespace Sandbox.Api.Services
+{
+    public class OlaMundoService : OlaMundo.OlaMundoBase
+    {
+        public override Task<OlaReply> Saudacao(OlaRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new OlaReply()
+            { Message = $"Bem vindo {request.Name}! Ola Mundo Grpc" });
+        }
+    }
+}
--- Sandbox.Console/Program.cs
+++ Sandbox.Console/Program.cs
@@ -1,1 +1,15 @@
-﻿System.Console.WriteLine("Hello World!");
+﻿using Grpc.Net.Client;
+using Sandbox.Protos;
+using System;
+
+using (var channel = GrpcChannel.ForAddress("https://localhost:5001/"))
+{
+    var client = new OlaMundo.OlaMundoClient(channel);
+
+    var reply = await client.SaudacaoAsync(new OlaRequest { Name = "Victor" });
+
+    System.Console.WriteLine(reply.Message);
+}
+
+
+
